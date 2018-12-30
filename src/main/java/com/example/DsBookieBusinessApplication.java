44d6f77bc874076299bf896446a92f15da949466
Dylan Rowley <dylan.rowley@ucdconnect.ie>
2018-12-30 22:21:53 +0000

    Population of Database now finished. Need to redo the horse racing once we confirm how many horses in each race etc.
    Must uncomment lines of code in DsBookieApplication and have DB running for it to populate.

--- src/main/java/com/example/DsBookieBusinessApplication.java
+++ src/main/java/com/example/DsBookieBusinessApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.dbsetup.PopulateDB;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +13,11 @@ import org.springframework.context.annotation.ComponentScan;
 public class DsBookieBusinessApplication {
 
    public static void main(String[] args) {
+
+//      PopulateDB
+//      PopulateDB dbPopulate = new PopulateDB();
+//      dbPopulate.populateDatabase();
+
       SpringApplication.run(DsBookieBusinessApplication.class, args);
    }
 
